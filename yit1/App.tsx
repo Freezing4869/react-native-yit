d37003f24ae9faf0ef04ffa16d7f34d8b7bf30be
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-08-22 12:04:17 +0700

    1234

--- yit1/App.tsx
+++ yit1/App.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 type Props = {};
 
 const App = (props: Props) => {
+  const textStyle = {backgroundColor: '#F00', color: '#FFF'};
+
   return (
     <View style={{flexDirection: 'column'}}>
-      <Text>1111</Text>
-      <Text>2222</Text>
-      <Text>3333</Text>
-      <Text>4444</Text>
+      <Text style={{...textStyle, backgroundColor: '#F00'}}>1111</Text>
+      <Text style={{...textStyle, backgroundColor: '#030'}}>2222</Text>
+      <Text style={{...textStyle, backgroundColor: '#0F0'}}>3333</Text>
+      <Text style={{...textStyle, backgroundColor: '#00F'}}>4444</Text>
     </View>
   );
 };
