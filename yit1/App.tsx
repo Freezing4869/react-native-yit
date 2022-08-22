2cd980e3dd11af75bd0a13efd75835f83aef6bc7
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-08-22 11:52:36 +0700

    1234

---
+++ yit1/App.tsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <View style={{flexDirection: 'column'}}>
+      <Text>1111</Text>
+      <Text>2222</Text>
+      <Text>3333</Text>
+      <Text>4444</Text>
+    </View>
+  );
+};
+
+export default App;
