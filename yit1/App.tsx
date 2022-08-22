0217f437fbfd63a4ede124c82e59cdc5ae00ab6b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-08-22 13:18:38 +0700

    1234

--- yit1/App.tsx
+++ yit1/App.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import {View, Text} from 'react-native';
 import React from 'react';
 
 type Props = {};
 
 const App = (props: Props) => {
-  const textStyle = {backgroundColor: '#F00', color: '#FFF'};
+  const textStyle = {backgroundColor: '#F00', color: '#FFF', fontSize: 30};
 
   return (
-    <View style={{flexDirection: 'column'}}>
+    <View
+      style={{flexDirection: 'column', backgroundColor: '#000', height: 300}}>
       <Text style={{...textStyle, backgroundColor: '#F00'}}>1111</Text>
       <Text style={{...textStyle, backgroundColor: '#030'}}>2222</Text>
       <Text style={{...textStyle, backgroundColor: '#0F0'}}>3333</Text>
